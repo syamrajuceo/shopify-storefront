5ae27cbd3c528d6f39f499efdd90d53129089661
Sooraj Rajkumar <iamsoorajrajkumar@gmail.com>
2024-12-16 23:06:12 +0530

    profile page updated

--- src/components/Profile/UserData.jsx
+++ src/components/Profile/UserData.jsx
@@ -5,7 +5,7 @@ function UserData({user}) {
   return (
     <>
       {user ? (
-        <div className="w-64 bg-gray-800 text-white p-4">
+        <div className="w-64 bg-gray-200 text-gray-800 p-4">
           <div className="flex items-center space-x-4 mb-8">
             <div className="relative">
               <img
@@ -13,11 +13,11 @@ function UserData({user}) {
                 src={user.profilePicture || "https://randomuser.me/api/portraits/men/1.jpg"}
                 alt="User Avatar"
               />
-              <span className="absolute bottom-0 right-0 w-4 h-4 bg-green-500 rounded-full border-2 border-white"></span>
+              {/* <span className="absolute bottom-0 right-0 w-4 h-4  rounded-full border-2 border-white"></span> */}
             </div>
             <div>
-              <h2 className="text-lg font-semibold">{`${user.firstName} ${user.lastName}` || "John Doe"}</h2>
-              <p className="text-sm text-gray-400">{"Online"}</p>
+              <h2 className="text-lg font-semibold">{`${user.firstName} ${user.lastName}` || "Unknown"}</h2>
+              {/* <p className="text-sm text-gray-400">{"Online"}</p> */}
             </div>
           </div>
         </div>
