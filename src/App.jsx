5bff238bef22cabfed9dccf58cb8012349c17823
Muhammed Salih Km <salihkm000@gmail.com>
2025-01-08 18:00:25 +0530

    eye test pop up added

--- src/App.jsx
+++ src/App.jsx
@@ -12,19 +12,21 @@ import { Loading } from "./components/loading/Loading";
 
 import useShopifyStore from "./store/useShopifyStore";
 import { fetchOrders } from "./store/orders";
+import PopupModal from "./components/testPopup/EyeTest";
 const App = () => {
   const setLoading = useShopifyStore((state) => state.setLoading);
   const loading = useShopifyStore((state) => state.loading);
+  const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
     const loadData = async () => {
       try {
         setLoading(true);
         const products = await fetchAllProducts();
+        setLoading(false);
         const cart = await fetchCart();
         const reviews = await fetchReviews();
-        setLoading(false);
-        const orders =await fetchOrders()
+        const orders = await fetchOrders();
         const fetchedCart = await fetchCart();
         const fetchedCollections = await fetchCollectionsWithProducts();
       } catch (error) {
@@ -34,8 +36,21 @@ const App = () => {
     loadData();
   }, []);
 
+  useEffect(() => {
+    const isPopupShown = localStorage.getItem("popupShown");
+    if (!isPopupShown) {
+      setShowPopup(true);
+      localStorage.setItem("popupShown", "true");
+    }
+  }, []);
+
+  const closeModal = () => {
+    setShowPopup(false);
+  };
+
   return (
     <>
+      {showPopup && <PopupModal closeModal={closeModal} />}
       <RouterProvider router={router} />
     </>
   );
