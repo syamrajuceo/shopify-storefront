2a0dead1436ecf77a16f5ff8d86441bd7f36a347
Muhammed Salih Km <salihkm000@gmail.com>
2025-02-15 11:17:43 +0530

    docker updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173,
+    hmr: {
+      clientPort: 443,
+    },
+  },
+  preview: {
+    port: 5173,
+    host: '0.0.0.0',
+  },
+});
