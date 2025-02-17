e667d4f53c42b2c54fbd10aa38696dcf54f6010d
Muhammed Salih Km <salihkm000@gmail.com>
2025-02-17 16:27:34 +0530

    booking link updated

--- src/components/testPopup/EyeTest.jsx
+++ src/components/testPopup/EyeTest.jsx
@@ -31,7 +31,7 @@ const PopupModal = ({ closeModal }) => {
         {/* Book Now Button */}
         <div className="mt-6 flex justify-center">
           <a
-            href="https://eyestorebooking.netlify.app"
+            href="https://booking.eyestore.ae/"
             target="_blank"
             rel="noopener noreferrer"
             className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-600"
